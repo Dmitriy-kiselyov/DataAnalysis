f500b7bf0f84375abae6b4658915c9f1c87be1c9
Dmitriy Kiselev <dmitriy.penetrator@gmail.com>
2019-12-25 10:46:00 +0300

    Topic3 done

--- DataAnalysis/Topic3.R
+++ DataAnalysis/Topic3.R
@@ -1,21 +1,88 @@
 # Используя средства предварительной обработки и дисперсионного анализа изучите данные выборки npk и установите значимость влияния факторов на вариацию зависимой переменной.
 
-# одномерный дисперсионный анализ ANOVA
+library(sm)
+
+# !!! Однофакторный дисперсионный анализ (One-Way ANOVA)
 # оценка влияния вариации одного или нескольких факторов на вариацию одной зависимой переменной Y
 
 View(npk)
 
-options(contrasts = c("contr.sum", "contr.poly"))
+# Модель включает один фактор и одну зависимую количественную переменную
+# yield – урожай (зависимая переменная, Y)
+# N – применение азота на участке (номинальная переменная, фактор)
 
-npk.aov <- aov(yield ~ block + N * P * K, npk)
-npk.aov
-summary(npk.aov)
-coef(npk.aov)
+View(npk[c('N', 'yield')])
 
-options(contrasts = c("contr.treatment", "contr.poly"))
+table(npk$N) # сколько наблюдений
 
-npk.aov1 <- aov(yield ~ block + N + K, data = npk)
-summary.lm(npk.aov1)
+lapply(list(yield ~ N, data = npk), summary) # описательная статистика
 
-se.contrast(npk.aov1, list(N == "0", N == "1"), data = npk)
-model.tables(npk.aov1, type = "means", se = TRUE)
+plot(yield ~ N, data = npk) # визуально средние
+
+# Для применения дисперсионного анализа должны выполняться условия нормального распределения
+
+# плотность yeild
+hist(npk$yield, breaks = 6, freq = FALSE, col = "lightblue", xlab = "yield", ylab = "", main = "Плотность yield")
+lines(density(npk$yield), col = "red", lwd = 2)
+
+# плотность yeild по группам
+attach(npk)
+sm.density.compare(yield, N, lwd = 2, xlab = "yield")
+title(main = "Плотность по N")
+legend("topright", levels(N), fill = c(2:5))
+
+# для подтверждения нормальной плотности тест Шапиро-Уилкса (p>0,05)
+shapiro.test(npk$yield)
+# для подтверждния однородной дисперсии (p>0,05)
+bartlett.test(yield ~ N, data = npk)
+
+# Дисперсионный анализ
+# проверим гипотезу о равенстве средних по группам
+# (различия между группами несущественны и вызваны влиянием случайных факторов)
+# Ключевая идея в сравнении вариации между группами с вариацией внутри групп, вносящими вклад в общую вариацию переменной
+# (p>0,05) - принятие
+
+summary(aov(yield ~ N, data = npk))
+# Отклоняем, значит азот влияет на результат
+
+# Линейная модель
+# Влияние - линия, смотрим отклонение
+summary(lm(yield ~ N, data = npk))
+
+# ВЫВОД: применение азота статистически значимо на вариацию урожайности
+
+# P.S. При невыполнении условий применимости дисперсионного анализа используют непараметрические методы
+oneway.test(yield ~ N, data = npk) # метод Уэлча
+kruskal.test(yield ~ N, data = npk) # тест Краскала-Уоллиса
+
+# !!! Двухфакторный дисперсионный анализ
+
+# Визуализация средних по блокам
+with(npk, interaction.plot(x.factor = block, trace.factor = N, response = yield, main = "N"))
+with(npk, interaction.plot(x.factor = block, trace.factor = P, response = yield, main = "P"))
+with(npk, interaction.plot(x.factor = block, trace.factor = K, response = yield, main = "K"))
+
+# Тест Бартлетта на однородность дисперсий (p > 0,05)
+bartlett.test(yield ~ N, data = npk)
+bartlett.test(yield ~ P, data = npk)
+bartlett.test(yield ~ K, data = npk)
+# все дисперсии однородны
+
+# Дисперсионный анализ (p < 0.05)
+summary(aov(yield ~ block + N * P * K, data = npk))
+# ВЫВОД: статистически значимы: N, K
+
+# Линейная модель
+# Если нарушается условие сбалансированности, следует применять линейную модель
+summary(lmnpk <- lm(yield ~ block + N * P * K, data = npk))
+# Значимость N=9.8500
+anova(lmnpk)
+# ВЫВОД: статистически значимы: N, K
+
+# Тесты Краскала-Уоллиса (p < 0.05)
+kruskal.test(yield ~ N, data = npk)
+kruskal.test(yield ~ P, data = npk)
+kruskal.test(yield ~ K, data = npk)
+# ВЫВОД: значим N
+
+# Общий вывод: N - значим, P и комбинации - не значимы
